f48cfd3656c768207a613bd11eec416a6a24941b
St4rJ1n <ojryu@pusan.ac.kr>
2024-05-07 15:09:40 +0900

    react tailwind

---
+++ src/01/hello.js
@@ -0,0 +1,14 @@
+function Hello() {
+    return (
+        <>
+        <p>
+            K-Digital 7기 RYU
+        </p>
+        <ul>
+            <li>React Component</li>
+        </ul>
+        </>         //공백 괄호. 묶어주는 태그 역할.
+    );
+}
+
+export default Hello ;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './01/hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <Hello />
+        <Hello />
+        <Hello />
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
