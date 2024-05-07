3f78f0aef9010561f4c0f1ee17d70c5b610d9711
St4rJ1n <ojryu@pusan.ac.kr>
2024-05-07 17:51:09 +0900

    0507update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import Hello from './01/hello';
+// import logo from './logo.svg' ;
+// import MyClock from './02/MyClock'; 
+// import { RiHomeHeartFill } from "react-icons/ri";
+// import MyDiv from './03/MyDiv'; 
+import MyList from './04/MyList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello />
-        <Hello />
-        <Hello />
-
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+    <div className="flex flex-col w-full max-w-screen-lg h-screen overflow-y-auto mx-auto">
+      <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-200'>
+        <p>리액트 실습</p> 
+        {/* <p><RiHomeHeartFill className='text-3xl text-green-900'/></p> */}
       </header>
+      <main className='grow flex justify-center items-center'>
+        {/* <div className='flex justify-center items-center w-1/3 h-1/2'>
+          <img src={logo} alt="logo" />
+        </div> */}
+
+        {/* <MyDiv /> */}
+        <MyList />
+      </main>
+      <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
+      ⓒ Ryu Eo Jin , K-Digital-7
+      </footer>
     </div>
   );
 }
