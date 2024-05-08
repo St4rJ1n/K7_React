d5cc673f2e4a01ba0b24405b07598ef99fcc16c6
St4rJ1n <ojryu@pusan.ac.kr>
2024-05-08 11:45:25 +0900

    04 05 UI  fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 // import logo from './logo.svg' ;
 // import MyClock from './02/MyClock'; 
-// import { RiHomeHeartFill } from "react-icons/ri";
+import { RiHomeHeartFill } from "react-icons/ri";
 // import MyDiv from './03/MyDiv'; 
-import MyList from './04/MyList';
+// import MyList from './04/MyList';
+import Lotto from './05/Lotto';
 
 function App() {
   return (
     <div className="flex flex-col w-full max-w-screen-lg h-screen overflow-y-auto mx-auto">
       <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-200'>
         <p>리액트 실습</p> 
-        {/* <p><RiHomeHeartFill className='text-3xl text-green-900'/></p> */}
+        <p><RiHomeHeartFill className='text-3xl text-green-900'/></p>
       </header>
       <main className='grow flex justify-center items-center'>
         {/* <div className='flex justify-center items-center w-1/3 h-1/2'>
@@ -18,7 +19,8 @@ function App() {
         </div> */}
 
         {/* <MyDiv /> */}
-        <MyList />
+        {/* <MyList /> */}
+        <Lotto />
       </main>
       <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
       ⓒ Ryu Eo Jin , K-Digital-7
