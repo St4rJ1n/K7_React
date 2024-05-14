9076213c62e5ce1860a4c88d774cc8903d033eda
St4rJ1n <ojryu@pusan.ac.kr>
2024-05-14 15:45:51 +0900

    TrafficMain

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import './App.css';
 // import logo from './logo.svg' ;
-// import MyClock from './02/MyClock'; 
+import MyClock from './02/MyClock'; 
 import { RiHomeHeartFill } from "react-icons/ri";
 // import MyDiv from './03/MyDiv'; 
 // import MyList from './04/MyList';
-// 
-import './06/BoxOffice';
+// import Lotto from './05/Lotto';
+// import BoxOffice from './06/BoxOffice';
+// import FoodMain from  './07/FoodMain' ;
+import TrafficMain from './08/TrafficMain';
 
 function App() {
   return (
-    <div className="flex flex-col w-full max-w-screen-lg h-screen overflow-y-auto mx-auto">
+    <div className="flex flex-col w-full max-w-screen-lg h-screen mx-auto">
       <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-200'>
         <p>리액트 실습</p> 
         <p><RiHomeHeartFill className='text-3xl text-green-900'/></p>
       </header>
-      <main className='grow flex justify-center items-center'>
+      <main className='grow w-full flex justify-center items-center overflow-y-auto '>
         {/* <div className='flex justify-center items-center w-1/3 h-1/2'>
           <img src={logo} alt="logo" />
         </div> */}
-
+        {/* <MyClock /> */}
         {/* <MyDiv /> */}
         {/* <MyList /> */}
-        <Lotto />
+        {/* <Lotto /> */}
+        {/* <BoxOffice /> */}
+        {/* <FoodMain /> */}
+        <TrafficMain />
       </main>
       <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
-      ⓒ Ryu Eo Jin , K-Digital-7
+      ⓒ Kim Kyung Min , K-digital-7
       </footer>
     </div>
   );
